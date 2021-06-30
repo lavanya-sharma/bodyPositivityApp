e616272512d8666538393ebfb13fd272edecb8d9
Lavanya Sharma <sharma.lavanya@icloud.com>
2021-06-30 13:02:55 -0700

    Added navigation bar & fixed color scheme

--- bodyPositivityApp/ViewController.swift
+++ bodyPositivityApp/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapButton()
+    {
+        
+        let vc = UIViewController()
+        vc.view.backgroundColor = #colorLiteral(red: 0.9766682982, green: 0.9284920096, blue: 0.8808675408, alpha: 1)
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    @IBAction func buttonTapped()
+    {
+        let vc = UIViewController()
+        vc.view.backgroundColor = #colorLiteral(red: 0.9766682982, green: 0.9284920096, blue: 0.8808675408, alpha: 1)
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 
 }
 
